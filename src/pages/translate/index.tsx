3090cd9dd9f47583b487e126b8d33133b6140462
FCA1978 <292409835@qq.com>
2023-09-09 11:34:46 +0800

    翻译页面修改样式

--- src/pages/translate/index.tsx
+++ src/pages/translate/index.tsx
@@ -1,9 +1,22 @@
+import { useNavigate } from "react-router-dom";
+import { Button } from "antd";
 import "./translate.styl";
 
 function Translate() {
+  // 路由钩子
+  const navigate = useNavigate();
   return (
     <div className="translateHome">
       <h1>Translate Page</h1>
+      <div className="ipt-con">
+        <Button
+          onClick={() => {
+            navigate("/login");
+          }}
+        >
+          返回登录
+        </Button>
+      </div>
     </div>
   );
 }
